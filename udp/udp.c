c4aa0c9ddd354018664921869a4518882aca73f6
JestrabikR <jestrab2003@gmail.com>
2024-03-31 14:35:38 +0200

    removed udp_connect

--- udp/udp.c
+++ udp/udp.c
@@ -1,14 +1,1 @@
 #include "udp.h"
-
-int udp_connect(int socket_fd, const char *ip, int port) {
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(port);
-    inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
-    
-    int ret_value = connect(socket_fd, (struct sockaddr *)&address, sizeof(address));
-    if (ret_value == -1) return 1;
-
-    printf("connected\n"); //TODO: REMOVE
-    return 0;
-}
--- udp/udp.h
+++ udp/udp.h
@@ -1,10 +1,4 @@
 #ifndef UDP_H
 #define UDP_H
 
-#include <stdio.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-int udp_connect(int socket_fd, const char *ip, int port);
-
 #endif
